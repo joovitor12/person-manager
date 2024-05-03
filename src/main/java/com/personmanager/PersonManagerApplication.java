e13849719af1ba731dcb4872bf2eef62b1b7c15b
joovitor12 <joao.machado@blip.ai>
2024-05-02 23:15:33 -0300

    feat: init project

---
+++ src/main/java/com/personmanager/PersonManagerApplication.java
@@ -0,0 +1,13 @@
+package com.personmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonManagerApplication.class, args);
+	}
+
+}
